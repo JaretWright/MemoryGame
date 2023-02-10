de585b899544e28995cb70f1ba2493c69245214e
JaretWright <jaret.wright@georgiancollege.ca>
2023-02-10 16:47:03 -0500

    Added the remaining controller logic for the game

--- src/main/java/com/example/memorygame/MemoryGameController.java
+++ src/main/java/com/example/memorygame/MemoryGameController.java
@@ -1,6 +1,5 @@
 package com.example.memorygame;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
@@ -9,6 +8,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.FlowPane;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 
 public class MemoryGameController implements Initializable {
@@ -22,14 +23,36 @@ public class MemoryGameController implements Initializable {
     @FXML
     private FlowPane imagesFlowPane;
 
+    private ArrayList<MemoryCard> cardsInGame;
+
+    private MemoryCard firstCard, secondCard;
+    private int numOfGuess;
+    private int numOfMatches;
+
     @FXML
-    void playAgain(ActionEvent event) {
+    void playAgain() {
+        firstCard=null;
+        secondCard =null;
+
+        DeckOfCards deck = new DeckOfCards();
+        deck.shuffle();
+        cardsInGame = new ArrayList<>();
+
+        for (int i =0; i<imagesFlowPane.getChildren().size()/2; i++)
+        {
+            Card cardDealt = deck.dealTopCard();
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(),cardDealt.getFaceName()));
+            cardsInGame.add(new MemoryCard(cardDealt.getSuit(),cardDealt.getFaceName()));
+        }
+        Collections.shuffle(cardsInGame);
+        flipAllCards();
 
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         initializeImageView();
+        playAgain();
     }
 
     /**
@@ -46,8 +69,65 @@ public class MemoryGameController implements Initializable {
 
             //register a click listener
             imageView.setOnMouseClicked(event -> {
-                System.out.println(imageView.getUserData());
+                flipCard((int) imageView.getUserData());
             });
         }
+    }
+
+    /**
+     * This will show the back of all cards that are not matched
+     */
+    private void flipAllCards()
+    {
+        for (int i=0; i<cardsInGame.size();i++)
+        {
+            ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(i);
+            MemoryCard card = cardsInGame.get(i);
+
+            if (card.isMatched())
+                imageView.setImage(card.getImage());
+            else
+                imageView.setImage(card.getBackOfCardImage());
+        }
+    }
+
+    private void flipCard(int indexOfCard)
+    {
+        if (firstCard==null && secondCard==null)
+            flipAllCards();
+
+        ImageView imageView = (ImageView) imagesFlowPane.getChildren().get(indexOfCard);
+
+        if (firstCard==null)
+        {
+            firstCard = cardsInGame.get(indexOfCard);
+            imageView.setImage(firstCard.getImage());
+        }
+        else if (secondCard==null)
+        {
+            numOfGuess++;
+            secondCard = cardsInGame.get(indexOfCard);
+            imageView.setImage(secondCard.getImage());
+            checkForMatch();
+            updateLabels();
+        }
+    }
+
+    private void updateLabels()
+    {
+        correctGuessesLabel.setText(Integer.toString(numOfMatches));
+        guessLabel.setText(Integer.toString(numOfGuess));
+    }
+
+    private void checkForMatch()
+    {
+        if (firstCard.isSameCard(secondCard))
+        {
+            numOfMatches++;
+            firstCard.setMatched(true);
+            secondCard.setMatched(true);
+        }
+        firstCard=null;
+        secondCard=null;
     }
 }
